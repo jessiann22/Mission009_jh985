2f937e5ec1a65651e6c2dd163c8f34ca97811a38
jessiann22 <Jessicaa4755@gmail.com>
2023-02-28 23:03:34 -0700

    Add project files.

---
+++ Mission009_jh985/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission009_jh985.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission009_jh985.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        //private BookstoreContext context { get; set; }
+        //public HomeController(BookstoreContext temp) => context = temp;
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+            
+
+            //var home = context.Books.ToList();
+            var home = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum-1) * pageSize)
+                .Take(pageSize);
+            return View(home);
+        }
+    }
+}
+/*
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+ */
---
+++ Mission009_jh985/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission009_jh985.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-blah")]
+    public class PaginationTagHelper : TagHelper
+    {
+        //dynamically create the page links bb
+    }
+}
