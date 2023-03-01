898e48e8f79979c52290502370915aab7d069c9b
jessiann22 <Jessicaa4755@gmail.com>
2023-03-01 11:56:43 -0700

    commitement

--- Mission009_jh985/Infrastructure/PaginationTagHelper.cs
+++ Mission009_jh985/Infrastructure/PaginationTagHelper.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Mission009_jh985.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +15,38 @@ namespace Mission009_jh985.Infrastructure
     public class PaginationTagHelper : TagHelper
     {
         //dynamically create the page links bb
+
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper (IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+
+        //Different than the View Context
+        public PageInfo PageBlah { get; set; }
+        public string PageAction { get; set; }
+        public override void Process (TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i < PageBlah.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+
+                tb.Attributes["href"] = uh.Action(PageAction, new  {pageNum = i });
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+            }
+
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
     }
 }
---
+++ Mission009_jh985/Models/ViewModels/BooksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission009_jh985.Models.ViewModels
+{
+    public class BooksViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+        public PageInfo Pageinfo { get; set; }
+    }
+}
