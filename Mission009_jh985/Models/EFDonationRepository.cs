96684017c2fb30c252e0a548778d58e1ac28024c
jessiann22 <Jessicaa4755@gmail.com>
2023-03-16 18:35:49 -0600

    commity

---
+++ Mission009_jh985/Models/EFDonationRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission009_jh985.Models
+{
+    public class EFDonationRepository : IDonationRepository
+    {
+    }
+}
